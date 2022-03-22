50d08ec8c23815efa31a3c66f3c5ed3faa82a594
ViacheslavNikitin666 <101802380+ViacheslavNikitin666@users.noreply.github.com>
2022-03-22 17:11:08 +0200

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,26 @@
+Function Block(letter, rate, used, father, code) {
+this.letter = letter;
+this.rate = rate;
+this.used = used;
+this.father = father;
+this.code = code;
+}
+
+function FindCode(block) {
+
+      if (tree[block.father].code != '') {
+      block.code = three[block.father].code + '1';
+   }
+       else {
+      if (block.letter == three [minIndex].letter) {
+      block.code = '0';
+   {
+       else  if (block.letter == tree[preminIndex].letter) {
+       block.code = '1';
+    }
+       else {
+       FindCode(tree[BlockFather]);
+       block.code = tree[block.father].code + '0';
+       }
+      }
+     }
